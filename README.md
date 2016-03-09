909c439896c10c0ce66cd262e2f048a37edd7bc8
germanio <german.hohmann@gmail.com>
2016-03-09 12:39:02 -0300

    Add use of suites in configuration and updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # protractor-examples
 Examples to practice implementing E2E tests with Protractor
+
+This command will run only the basic tutorial section of the tests:
+
+<pre><code>protractor conf.js --suite basic_tutorial</code></pre>
+
+Additionally, you can run specific suites of tests with the command:
+
+<pre><code>protractor conf.js --suite basic_tutorial,improved_tests</code></pre>
--- conf.js
+++ conf.js
@@ -1,13 +1,20 @@
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['basic-tutorial/spec*.js', 'improved-tests/*-spec.js'],
+//    specs: ['basic-tutorial/spec*.js', 'improved-tests/*-spec.js'],
     capabilities: {
         browserName: 'firefox',
-    }
+    },
 //    multiCapabilities: [{
 //        browserName: 'firefox'
 //    }, {
 //        browserName: 'chrome'
-//    }]
+//    }],
+    suites: {
+        basic_tutorial: 'basic-totorial/**/spec*.js',
+        improved_tests: 'improved-tests/**/*-spec.js'
+    },
+    jasmineNodeOpts: {
+        showColors: true
+    }
 };
